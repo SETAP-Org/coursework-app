14d480977d644082cea8a03c7748bc8518ad9d23
Jay McKerracher <jaymckerracher@gmail.com>
2026-03-04 14:35:24 +0000

    make sure users refresh on index.js load

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,7 +1,11 @@
 import { getAllUsersModel, postUserModel } from "../models/userModels.js";
 
 export async function getAllUsersController(req, res) {
-    res.json(await getAllUsersModel());
+    try {
+        res.json(await getAllUsersModel());
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 }
 
 export async function postUserController(req, res) {
