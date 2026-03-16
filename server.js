469681a814ecbd0519fff60307bb8f60422ffa6a
Jay McKerracher <jaymckerracher@gmail.com>
2026-03-16 14:00:50 +0000

    setting up user session

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 import express from "express";
 import path from "path";
+import session from "express-session";
+import passport from "passport";
+import "dotenv/config";
+import "auth.js";
+
 import { getAllUsersController, postUserController } from "./controllers/userControllers.js";
 
 const __dirname = import.meta.dirname;
@@ -7,8 +12,25 @@ const __dirname = import.meta.dirname;
 const app = express();
 
 const port = 3000;
+
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json()); // Add this line
+app.use(express.json());
+
+app.use(session({
+  secret: process.env.session.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+    done(null, obj);
+});
 
 // paths to navigate pages
 app.get("/", async (req, res) => {
