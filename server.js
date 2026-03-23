c467ac98933aa356b583a087a32390a79c1684b7
Jay McKerracher <jaymckerracher@gmail.com>
2026-03-23 13:51:50 +0000

    format session and passport connections

--- server.js
+++ server.js
@@ -3,16 +3,14 @@ import path from "path";
 import session from "express-session";
 import passport from "passport";
 import dotenv from 'dotenv';
-import "./auth.js";
-
-dotenv.config({path: ".env.session-secret"});
-
+import { Strategy as MicrosoftStrategy } from "passport-microsoft";
 import { getAllUsersController, postUserController } from "./controllers/userControllers.js";
 
+dotenv.config({path: ".env.auth"})
+dotenv.config({path: ".env.session-secret"});
+
 const __dirname = import.meta.dirname;
-
 const app = express();
-
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -23,15 +21,34 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }));
+
+passport.use(new MicrosoftStrategy({
+		clientID: process.env.CLIENT_ID,
+		clientSecret: process.env.CLIENT_SECRET,
+		callbackURL: "http://localhost:3000/api/auth/callback",
+		scope: ["User.Read"]
+	},
+	function(accessToken, refreshToken, profile, done) {
+		return done(null, profile);
+	})
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.serializeUser((user, done) => {
-    done(null, user);
+  const filteredUser = {
+    microsoftId: user.id,
+    firstName: user.name.givenName,
+    lastName: user.name.familyName,
+    email: user.emails[0].value
+  }
+  done(null, filteredUser);
 });
 
 passport.deserializeUser((obj, done) => {
-    done(null, obj);
+  console.log(obj);
+  done(null, obj);
 });
 
 // paths to navigate pages
@@ -59,6 +76,6 @@ app.get("/api/auth/callback", passport.authenticate("microsoft", {
   }),
   (req, res) => {
 	res.redirect("/user-dashboard");
-	  console.log(req.user);
+    console.log("Dummy Log");
   }
 );
