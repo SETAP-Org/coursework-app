dcf24470a778bd15ae7923c212666940150f2ddc
Harry <hfaulkner2006@gmail.com>
2026-03-23 13:23:35 +0000

    Begin simplfying auth code

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ import express from "express";
 import path from "path";
 import session from "express-session";
 import passport from "passport";
-import dotenv from 'dotenv';
-import "./auth.js";
+import dotenv from "dotenv";
+import { Strategy as MicrosoftStrategy } from "passport-microsoft";
+import {
+  getAllUsersController,
+  postUserController,
+} from "./controllers/userControllers.js";
 
-dotenv.config({path: ".env.session-secret"});
-
-import { getAllUsersController, postUserController } from "./controllers/userControllers.js";
+dotenv.config({ path: ".env.auth" });
+dotenv.config({ path: ".env.session-secret" });
 
 const __dirname = import.meta.dirname;
 
@@ -18,20 +21,37 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+
+passport.use(
+  new MicrosoftStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/api/auth/callback",
+      scope: ["User.Read"],
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    },
+  ),
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.serializeUser((user, done) => {
-    done(null, user);
+  done(null, user);
 });
 
 passport.deserializeUser((obj, done) => {
-    done(null, obj);
+  done(null, obj);
 });
 
 // paths to navigate pages
@@ -48,17 +68,19 @@ app.listen(port, () => {
 });
 
 // API routes
-app.get("/api/users/all", getAllUsersController)
+app.get("/api/users/all", getAllUsersController);
 
-app.post("/api/users/postUser", postUserController)
+app.post("/api/users/postUser", postUserController);
 
 app.get("/api/auth", passport.authenticate("microsoft"));
 
-app.get("/api/auth/callback", passport.authenticate("microsoft", {
-    failureRedirect: "/"
+app.get(
+  "/api/auth/callback",
+  passport.authenticate("microsoft", {
+    failureRedirect: "/",
   }),
   (req, res) => {
-	res.redirect("/user-dashboard");
-	  console.log(req.user);
-  }
+    res.redirect("/user-dashboard");
+    console.log(req.user);
+  },
 );
