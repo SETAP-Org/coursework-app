a1624f6fefa73923944b5dcb8f912cf68c175436
Jay McKerracher <jaymckerracher@gmail.com>
2026-03-24 14:25:01 +0000

    add comments to server.js

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
+// package imports
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
+import passport from "passport";
 
+// controller imports
 import {
   serveLanding,
   serveUserDashboard,
 } from "./controllers/serveControllers.js";
-
 import {
   getAllUsersController,
   postUserController,
 } from "./controllers/userControllers.js";
+
+// util imports
 import createSession from "./utils/session.js";
 import { connectMicrosoft } from "./utils/auth.js";
 
-import passport from "passport";
-
+// configfure environment variables
 dotenv.config({ path: ".env.auth" });
 
+// server configuration
 const __dirname = import.meta.dirname;
 const app = express();
 const port = 3000;
 
+// middleware
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-
 createSession(app);
 connectMicrosoft(app);
 
