4239aec6a1a05522234eeae8b331d6a6319fb1a5
Harry <hfaulkner2006@gmail.com>
2026-04-13 11:01:07 +0100

    Normalize indentation and use querySelector

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -1,24 +1,24 @@
 async function userNav() {
-    const userDataJson = await fetch("/api/me");
-    const userData = await userDataJson.json();
-    const dbUserData = userData.dbUser;
+  const userDataJson = await fetch("/api/me");
+  const userData = await userDataJson.json();
+  const dbUserData = userData.dbUser;
 
-    const projectsBtn = document.querySelector("#projects-button");
-    const profileBtn = document.querySelector("#profile-button");
-    // const burger = document.querySelector("#nav-burger");
+  const projectsBtn = document.querySelector("#projects-button");
+  const profileBtn = document.querySelector("#profile-button");
+  // const burger = document.querySelector("#nav-burger");
 
-    projectsBtn.href = `/${dbUserData.username}/projects`;
-    profileBtn.href = `/${dbUserData.username}/profile`;
+  projectsBtn.href = `/${dbUserData.username}/projects`;
+  profileBtn.href = `/${dbUserData.username}/profile`;
 
-    // burger.addEventListener("click", () => {
-    //     var x = document.getElementById("MyHeader");
+  // burger.addEventListener("click", () => {
+  //     var x = document.querySelector("#MyHeader");
 
-    //     if (x.className === "Dashboard-header") {
-    //         x.className += " responsive";
-    //     } else {
-    //         x.className = "Dashboard-header";
-    //     }
-    // })
+  //     if (x.className === "Dashboard-header") {
+  //         x.className += " responsive";
+  //     } else {
+  //         x.className = "Dashboard-header";
+  //     }
+  // })
 }
 
 userNav();
