4f0dd86360fe68d5810cb8101cd5c89fbc4087aa
Jay McKerracher <jaymckerracher@gmail.com>
2026-04-15 03:55:11 +0100

    implement message formatting and display in chat with timestamps

--- public/scripts/projectChat.js
+++ public/scripts/projectChat.js
@@ -1,7 +1,26 @@
+// function to format time given from message in database
+function formatTime(timestamp) {
+    // 2026-04-14T22:50:32.656Z
+
+    const splitDate = timestamp.split("T")[0].split("-");
+    const splitTime = timestamp.split("T")[1].split(":");
+
+    const formattedDate = `${splitDate[2]}/${splitDate[1]}/${splitDate[0]}`;
+    const formattedTime = `${splitTime[0]}:${splitTime[1]}`;
+
+    return {
+        date: formattedDate,
+        time: formattedTime,
+    }
+}
+
 async function projectChat() {
-    // loading the messages
-    const response = await fetch("/api/chat");
-    const { messages } = await response.json();
+    // loading the messages and the user
+    const chatResponse = await fetch("/api/chat");
+    const { messages } = await chatResponse.json();
+
+    const meResponse = await fetch("/api/me");
+    const { dbUser } = await meResponse.json();
 
     // get a hold of the elements that you are trying to access
     const messageForm = document.querySelector(".chat-form");
@@ -10,6 +29,49 @@ async function projectChat() {
     const sendBtn = document.querySelector(".chat-send");
     const messagesContainer = document.querySelector(".chat-messages");
     let messageContent = "";
+
+    // messages container population
+    for (const message of messages) {
+        // creating the elements
+        const container = document.createElement("div");
+        const infoTop = document.createElement("p");
+        const bubble = document.createElement("div");
+        const content = document.createElement("p");
+        const infoBottom = document.createElement("p");
+
+        // configuring the elements
+        content.innerText = message.message_content
+
+        if (message.sender_id != dbUser.user_id) {
+            infoTop.innerText = `Sent by ${message.sender_id}`
+            container.className = "message-container-left";
+            infoTop.className = "message-info-left";
+            infoBottom.className = "message-info-left";
+            bubble.className = "message-bubble-left";
+            content.className = "message-content-left";
+        } else {
+            container.className = "message-container-right";
+            infoTop.className = "message-info-right";
+            infoBottom.className = "message-info-right";
+            bubble.className = "message-bubble-right";
+            content.className = "message-content-right";
+        }
+
+        const {date, time} = formatTime(message.m_date_sent);
+
+        infoBottom.innerText = `${date} | ${time}`;
+
+        // composing elements
+        if (message.sender_id != dbUser.user_id) {
+            container.appendChild(infoTop);
+        }
+        container.appendChild(bubble);
+        container.appendChild(infoBottom)
+        bubble.appendChild(content);
+
+        // adding the message to message container
+        messagesContainer.appendChild(container);
+    }
 
     // form event listener
     messageForm.addEventListener("submit", async (e) => {
