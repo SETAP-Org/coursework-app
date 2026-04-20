6732e9402bb4a6225a0c13f765f5ea206240a003
Harry <hfaulkner2006@gmail.com>
2026-04-20 12:31:53 +0100

    Normalize formatting in authControllers.js
    Reformat indentation and line breaks for readability and add missing
    newline at EOF. No behavior changes.

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -17,9 +17,9 @@ export async function checkIfLoggedInRedirect(req, res, next) {
       req.user.accessToken &&
       dbUser &&
       dbUser.username == req.params.username
-    ) next();
+    )
+      next();
     else res.redirect("/");
-
   } catch (err) {
     console.error("checkIfLoggedInRedirect error:", err);
     res.redirect("/");
@@ -28,12 +28,11 @@ export async function checkIfLoggedInRedirect(req, res, next) {
 
 // function to navigate to user dashboard if user already signed in
 export async function checkIfLoggedIn(req, res, next) {
-    if (req.user && req.user.accessToken) {
-        const dbUserResult = await getUserByMicrosoftIdModel(req.user.microsoftId);
-        const dbUser = dbUserResult.rows[0];
-        return res.redirect(`/${dbUser.username}`);
-    }
-    else next();
+  if (req.user && req.user.accessToken) {
+    const dbUserResult = await getUserByMicrosoftIdModel(req.user.microsoftId);
+    const dbUser = dbUserResult.rows[0];
+    return res.redirect(`/${dbUser.username}`);
+  } else next();
 }
 
 // function to sign out user on request
