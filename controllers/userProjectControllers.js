adc7ef3a63ab200f6ab05f9576e1e372c6e2e3de
Jay McKerracher <jaymckerracher@gmail.com>
2026-04-22 03:01:46 +0100

    add controller to add a user to a project

--- controllers/userProjectControllers.js
+++ controllers/userProjectControllers.js
@@ -1,5 +1,6 @@
 import { deleteUserFromProjectModel } from "../models/userProjectModels.js";
 
+// function to remove a user from a project
 export async function removeUserFromProject(req, res, next) {
     try {
         const { userId, projectId } = req.body;
@@ -11,6 +12,30 @@ export async function removeUserFromProject(req, res, next) {
             })
         } else {
             res.status(404).json({
+                success: false,
+            })
+        }
+    } catch(err) {
+        console.error("Error with removeUserFromProject:", err);
+
+        res.status(400).json({
+            success: false,
+        })
+    }
+}
+
+// function to add a user to a project
+export async function addUserToProject(req, res, next) {
+    try {
+        const { userId, projectId } = req.body;
+        const data = await postUserToProjectModel(userId, projectId);
+
+        if (data.rows[0].length > 0) {
+            res.status(200).json({
+                success: true,
+            })
+        } else {
+            res.status(400).json({
                 success: false,
             })
         }
