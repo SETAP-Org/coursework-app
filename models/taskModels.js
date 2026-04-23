553ada7d70aeb162884fccca1d1aaeaae48da657
Harry <hfaulkner2006@gmail.com>
2026-04-23 10:44:25 +0100

    Add updateTaskStatusModel function

--- models/taskModels.js
+++ models/taskModels.js
@@ -26,6 +26,19 @@ export async function postTaskModel(
   );
 }
 
+// ALTER
+export async function updateTaskStatusModel(taskId, projectId, status) {
+  return await query(
+    `
+    UPDATE tasks
+    SET tast_status = $3, t_time_updated = NOW()
+    WHERE task_id = $1 AND project_id = $2
+    RETURNING *;
+    `,
+    [taskId, projectId, status],
+  );
+}
+
 // READ
 export async function getTasksByProjectIdModel(projectId) {
   return await query(
