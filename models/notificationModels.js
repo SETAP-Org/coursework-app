816449210bff0974279b6d9c424fa95a286ea300
Jay McKerracher <jaymckerracher@gmail.com>
2026-04-24 18:52:22 +0100

    refactor postNotificationModel to remove task_id parameter

--- models/notificationModels.js
+++ models/notificationModels.js
@@ -1,25 +1,14 @@
 import { query } from "../db/connection";
 
-// export async function postMessageModel(senderId, projectId, message) {
-//     return await query(
-//         `
-//         INSERT INTO messages(sender_id, project_id, message_content, m_date_sent)
-//         VALUES ($1, $2, $3, NOW())
-//         RETURNING *;
-//         `,
-//         [senderId, projectId, message],
-//     );
-// }
-
 // CREATE
-export async function postNotificationModel(user_id, project_id, task_id, notification_type, notification_message) {
+export async function postNotificationModel(user_id, project_id, notification_type, notification_message) {
     return await query(
         `
-        INSERT INTO notifications (user_id, project_id, task_id, notification_type, notification_message, n_date_created)
-        VALUES ($1, $2, $3, $4, $5, NOW())
+        INSERT INTO notifications (user_id, project_id, notification_type, notification_message, n_date_created)
+        VALUES ($1, $2, $3, $4, NOW())
         RETURNING *;
         `,
-        [user_id, project_id, task_id, notification_type, notification_message],
+        [user_id, project_id, notification_type, notification_message],
     );
 }
 
