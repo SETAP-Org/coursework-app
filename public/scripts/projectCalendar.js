ea77fff83471cf533ce7dfb53aa602732aadec35
Jay McKerracher <jaymckerracher@gmail.com>
2026-05-10 19:16:29 +0100

    code cleanup to remove comments and reorganise file naming

--- public/scripts/projectCalendar.js
+++ public/scripts/projectCalendar.js
@@ -1,25 +1,20 @@
-import { formatTime } from "/scripts/utils.js";
-
 const socket = io();
 
 function projectCalendar() {
+  // show loading screen
   const loading = document.querySelector(".loading");
   loading.style.display = "flex";
 
   // ejs values
   const { meetings, projectId, groupUsers, userId, username, projectName } = window.scriptData;
 
-  console.log(groupUsers, 'these are the group users....')
-
+  // getting dom elements
   const eventsList = document.querySelector("#events-list");
   const eventTemplate = document.querySelector(".event-template");
-  // const eventForm = document.querySelector("#event-form");
-  // const eventFormSubject = document.querySelector("#event-form-subject");
-  // const eventFormStart = document.querySelector("#event-form-start");
-  // const eventFormEnd = document.querySelector("#event-form-end");
-  // const eventFormDescription = document.querySelector(
-  //   "#event-form-description",
-  // );
+  const createMeetingDialog = document.querySelector("#create-meeting-dialog");
+  const dialogForm = document.querySelector("#create-meeting-dialog form");
+  const createMeetingButton = document.querySelector("#create-meeting-button");
+  const dialogCloseButton = document.querySelector(".modal-close");
 
   // add full calendar modal to the ui
   const calendarEl = document.querySelector('#calendar');
@@ -27,52 +22,6 @@ function projectCalendar() {
       initialView: 'dayGridMonth'
   });
   calendar.render();
-
-  // function to add event to ui
-  // function addEventToUi(event) {
-  //   // clone the template
-  //   const clone = eventTemplate.content.cloneNode(true);
-  //   const cloneCard = clone.querySelector(".event-card");
-  //   const cloneHeading = clone.querySelector(".event-heading");
-  //   const cloneStart = clone.querySelector(".event-start");
-  //   const cloneEnd = clone.querySelector(".event-end");
-  //   const cloneDescription = clone.querySelector(".event-description");
-  //   const cloneButton = clone.querySelector(".event-delete-button");
-
-  //   // update the values
-  //   cloneHeading.innerText = event.subject;
-  //   cloneStart.innerText = event.start.dateTime;
-  //   cloneEnd.innerText = event.end.dateTime;
-  //   // cloneDescription.innerText = event.body?.content || '';
-
-  //   // event handler to delete event
-  //   cloneButton.addEventListener("click", async () => {
-  //     // show the loading screen
-  //     loading.style.display = "flex";
-
-  //     // delete from database
-  //     const response = await fetch(`/api/calendar/events/${event.id}`, {
-  //       method: "DELETE",
-  //     });
-  //     const data = await response.json();
-
-  //     // if success, remove from the ui
-  //     if (data.success) {
-  //       cloneCard.remove();
-  //       // toastify that event has been removed
-  //     }
-  //     // else, toastify to alert that deletion failed
-  //     else {
-  //       alert("Event failed to be deleted, please try again...");
-  //     }
-
-  //     // hide the loading screen
-  //     loading.style.display = "none";
-  //   });
-
-  //   // append to the relevant list
-  //   eventsList.appendChild(clone);
-  // }
 
   // function to add event to the ui
   function addEventToUi(title, start, end, description) {
@@ -84,8 +33,6 @@ function projectCalendar() {
     });
   }
 
-  console.log(meetings, 'this is the meetings')
-
   // populating the calendar
   for (const meeting of meetings) {
     addEventToUi(
@@ -96,6 +43,7 @@ function projectCalendar() {
     );
   }
 
+  // adds meeting to calendar when new meeting is added
   socket.on("meeting", (meeting) => {
     addEventToUi(
       meeting.meeting_subject,
@@ -105,60 +53,14 @@ function projectCalendar() {
     )
   })
 
-  // // populating the ui with the events
-  // if (!events || events.length === 0) {
-  //   eventsList.innerHTML = "<p>No upcoming events</p>";
-  // } else {
-  //   for (const event of events) addEventToUi(event);
-  // }
-
-  // event handler to add a new event when submitting a form
-  // eventForm.addEventListener("submit", async (e) => {
-  //   e.preventDefault();
-
-  //   // show the loading screen
-  //   loading.style.display = "flex";
-
-  //   // attempt to add the event to the database
-  //   const response = await fetch("/api/calendar/events", {
-  //     method: "POST",
-  //     headers: { "content-type": "application/json" },
-  //     body: JSON.stringify({
-  //       subject: eventFormSubject.value,
-  //       start: eventFormStart.value,
-  //       end: eventFormEnd.value,
-  //       description: eventFormDescription.value,
-  //     }),
-  //   });
-
-  //   const data = await response.json();
-
-  //   // if success, add event to ui
-  //   if (data.success) {
-  //     addEventToUi(data.event);
-  //   }
-  //   // else, toastify
-  //   else {
-  //     alert("Failed to add event to database....");
-  //   }
-
-  //   // hide the loading screen
-  //   loading.style.display = "none";
-  // });
-
-  // Dialog Logic
-  const createMeetingDialog = document.querySelector("#create-meeting-dialog");
-  const dialogForm = document.querySelector("#create-meeting-dialog form");
-  const createMeetingButton = document.querySelector("#create-meeting-button");
-  const dialogCloseButton = document.querySelector(".modal-close");
-
-  function toggleNewTaskForm() {
+  // function to show or hide new meeting dialog
+  function toggleNewMeetingForm() {
     const dialog = document.querySelector("#create-meeting-dialog");
     dialog.open ? dialog.close() : dialog.showModal();
   }
 
-  createMeetingButton.addEventListener("click", toggleNewTaskForm);
-  dialogCloseButton.addEventListener("click", toggleNewTaskForm);
+  createMeetingButton.addEventListener("click", toggleNewMeetingForm);
+  dialogCloseButton.addEventListener("click", toggleNewMeetingForm);
 
   // If click outside dialog, close
   if (createMeetingDialog) {
@@ -167,10 +69,12 @@ function projectCalendar() {
     });
   }
 
+  // event listener to handle new meeting submit
   if (dialogForm) {
     dialogForm.addEventListener("submit", async (e) => {
       e.preventDefault();
 
+      // getting the values from the form
       const form = e.target;
       const payload = {
         meetingSubject: form["meetingSubject"].value,
@@ -179,14 +83,13 @@ function projectCalendar() {
         meetingEnd: form["meetingEnd"].value,
       };
 
+      // formatting the date
       const startDateObj = new Date(payload.meetingStart);
       const endDateObj = new Date(payload.meetingEnd);
-
       const startDateTz = new Date(payload.meetingStart).toISOString();
       const endDateTz = new Date(payload.meetingEnd).toISOString();
 
-      console.log(startDateObj, payload.meetingStart, 'These are the dates...')
-
+      // exit submit if relevant time conditions are not met
       if (
         startDateObj > endDateObj ||
         startDateObj <= new Date() ||
@@ -197,6 +100,7 @@ function projectCalendar() {
         );
         return;
       }
+
 
       try {
         // send a meeting to the web socket
@@ -226,7 +130,7 @@ function projectCalendar() {
     });
   }
 
-  loadEvents();
+  // hide loading screen
   loading.style.display = "none";
 }
 
