129e8a7b80d038fc33c4bf0e5f9d1bce58614204
Jay McKerracher <jaymckerracher@gmail.com>
2026-05-11 22:01:16 +0100

    update configuration of app to work with testing

--- new_tests/ur1.test.js
+++
@@ -1,6 +0,0 @@
-import request from "supertest";
-import { app } from "../app.js";
-
-describe("UR1: Users should be able to authenticate with their Microsoft account", () => {
-    test("FR1: The system should allow users to register an account via Microsoft credentials", async () => {});
-});
