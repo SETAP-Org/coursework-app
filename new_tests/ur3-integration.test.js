9c8234fbe3ddd9e21c708865192ae3fa377dadbf
Jay McKerracher <jaymckerracher@gmail.com>
2026-05-11 23:55:14 +0100

    add test for team leader assignment to fail when database error occurs

---
+++ new_tests/ur3-integration.test.js
@@ -0,0 +1,36 @@
+import { jest } from "@jest/globals";
+import request from "supertest";
+import { query } from "../db/connection.js";
+
+jest.unstable_mockModule('../models/projectModels.js', () => ({
+    ...jest.requireActual('../models/projectModels.js'),
+    putTeamLeader: jest.fn()
+}));
+
+test('Should fail after error from model to update team leader', async () => {
+    const { default: app } = await import("../app.js");
+    const projectModels = await import('../models/projectModels.js');
+
+    // Mock putTeamLeader to throw error
+    projectModels.putTeamLeader.mockImplementation(() => {
+        throw new Error('DB Error');
+    });
+
+    // get the relevant database data
+    const bobRes = await query("SELECT user_id FROM users WHERE username = 'bob'");
+    const projectRes = await query("SELECT project_id FROM projects WHERE project_name = 'Test Project'");
+
+    // attempting to change team leader
+    const newLeaderId = bobRes.rows[0].user_id;
+    const projectId = projectRes.rows[0].project_id;
+
+    const response = await request(app)
+        .put("/api/projects/leader")
+        .send({ newLeaderId, projectId });
+
+    // expecting failure
+    expect(response.status).toBe(500);
+    expect(response.body.message).toMatch("Database error");
+
+    projectModels.putTeamLeader.mockReset();
+});
