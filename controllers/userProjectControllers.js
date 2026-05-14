fb4bf18264100afa1543b7dbc418fce86ba97fed
Jay McKerracher <jaymckerracher@gmail.com>
2026-05-14 23:43:29 +0100

    refactor addUserToProject function to improve request validation and error handling

--- controllers/userProjectControllers.js
+++ controllers/userProjectControllers.js
@@ -31,38 +31,51 @@ export async function removeUserFromProject(req, res, next) {
 // function to add a user to a project
 export async function addUserToProject(req, res, next) {
     try {
+        if (!req.body) {
+            return res.status(400).json({
+                success: false,
+                message: "No request body"
+            })
+        }
+
+        if (!req.body.username) {
+            return res.status(400).json({
+                success: false,
+                message: "No username provided"
+            })
+        }
+
+        if (!req.body.projectId) {
+            return res.status(400).json({
+                success: false,
+                message: "No project ID provided"
+            })
+        }
+
         const { username, projectId } = req.body;
 
         // check if user actually exists
         const userData = await getUserByUsernameModel(username);
 
         if (userData.rows.length === 0) {
-            res.status(400).json({
+            return res.status(400).json({
                 success: false,
                 message: "User does not exist!"
             })
         }
 
+        // add the user to the project
         const data = await postUserToProjectModel(userData.rows[0].user_id, projectId);
 
-        if (data.rows.length > 0) {
-            res.status(200).json({
-                success: true,
-                message: "User successfully added to the project!",
-                userId: userData.rows[0].user_id
-            })
-        } else {
-            res.status(400).json({
-                success: false,
-                message: "Something went wrong!"
-            })
-        }
+        res.status(200).json({
+            success: true,
+            message: "User successfully added to the project!",
+            userId: userData.rows[0].user_id
+        });
     } catch(err) {
-        console.error("Error with removeUserFromProject:", err);
-        
-        res.status(400).json({
+        res.status(500).json({
             success: false,
-            message: err
-        })
+            message: "Database error"
+        });
     }
 }
