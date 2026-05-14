7eee25425f29d19e2b98810edbdfe9dc9a96d751
Jay McKerracher <jaymckerracher@gmail.com>
2026-05-15 00:44:21 +0100

    refactor removeUserFromProject function to improve request validation and error handling

--- controllers/userProjectControllers.js
+++ controllers/userProjectControllers.js
@@ -4,26 +4,38 @@ import { getUserByUsernameModel } from "../models/userModels.js";
 // function to remove a user from a project
 export async function removeUserFromProject(req, res, next) {
     try {
+        if (!req.body) {
+            return res.status(400).json({
+                success: false,
+                message: "No request body"
+            })
+        }
+
+        if (!req.body.userId) {
+            return res.status(400).json({
+                success: false,
+                message: "No user ID provided"
+            })
+        }
+
+        if (!req.body.projectId) {
+            return res.status(400).json({
+                success: false,
+                message: "No project ID provided"
+            })
+        }
+
         const { userId, projectId } = req.body;
         const data = await deleteUserFromProjectModel(userId, projectId);
 
-        if (data.rows.length > 0) {
-            res.status(200).json({
-                success: true,
-                message: "You have been removed from this project!"
-            })
-        } else {
-            res.status(400).json({
-                success: false,
-                message: "Something went wrong!"
-            })
-        }
+        res.status(200).json({
+            success: true,
+            message: "You have been removed from this project!"
+        })
     } catch(err) {
-        console.error("Error with removeUserFromProject:", err);
-
-        res.status(400).json({
+        res.status(500).json({
             success: false,
-            message: err
+            message: "Database error"
         })
     }
 }
