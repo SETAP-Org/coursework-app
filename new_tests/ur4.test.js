cd2819cd43abbd4feb1a921db101dda76239b358
Jay McKerracher <jaymckerracher@gmail.com>
2026-05-15 00:50:14 +0100

    add tests for removeUserFromProject function to validate user removal and handle missing request body

--- new_tests/ur4.test.js
+++ new_tests/ur4.test.js
@@ -1,6 +1,7 @@
 // User Requirement 4:  Authenticated users assigned as a team leader should be able to manage members for their projects
 
 import { describe, jest, test } from "@jest/globals";
+import { deleteUserFromProjectModel } from "../models/userProjectModels.js";
 
 jest.unstable_mockModule('../models/userModels.js', () => ({
     ...jest.requireActual('../models/userModels.js'),
@@ -10,6 +11,7 @@ jest.unstable_mockModule('../models/userModels.js', () => ({
 jest.unstable_mockModule('../models/userProjectModels.js', () => ({
     ...jest.requireActual('../models/userProjectModels.js'),
     postUserToProjectModel: jest.fn(),
+    deleteUserFromProjectModel: jest.fn(),
 }));
 
 describe('addUserToProject', () => {
@@ -234,5 +236,58 @@ describe('addUserToProject', () => {
 
         userModels.getUserByUsernameModel.mockReset();
         userProjectModels.postUserToProjectModel.mockReset();
+    });
+});
+
+describe('removeUserFromProject', () => {
+    test('Should call the deleteUserFromProject() function and return a successful response', async () => {
+        const { removeUserFromProject } = await import('../controllers/userProjectControllers.js');
+        const userProjectModels = await import('../models/userProjectModels.js');
+
+        const req = {
+            body: {
+                userId: 'myUserId',
+                projectId: 'myProjectId'
+            }
+        }
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+        const next = jest.fn();
+
+        await removeUserFromProject(req, res, next);
+
+        expect(userProjectModels.deleteUserFromProjectModel).toHaveBeenCalled();
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({
+            success: true,
+            message: "You have been removed from this project!"
+        });
+
+        userProjectModels.deleteUserFromProjectModel.mockReset();
+    });
+
+    test('Should return the correct response if no request body is attached', async () => {
+        const { removeUserFromProject } = await import('../controllers/userProjectControllers.js');
+        const userProjectModels = await import('../models/userProjectModels.js');
+
+        const req = {}
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+        const next = jest.fn();
+
+        await removeUserFromProject(req, res, next);
+
+        expect(userProjectModels.deleteUserFromProjectModel).not.toHaveBeenCalled();
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({
+            success: false,
+            message: "No request body"
+        });
+
+        userProjectModels.deleteUserFromProjectModel.mockReset();
     });
 });
