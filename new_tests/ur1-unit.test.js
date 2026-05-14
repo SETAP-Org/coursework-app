03f95d9d697c2af3abf85d7155b57f20e6cc9aac
Jay McKerracher <jaymckerracher@gmail.com>
2026-05-14 02:40:13 +0100

    change file name for new integration file, comment out console logs that cause errors during testing

--- new_tests/ur1-unit.test.js
+++ new_tests/ur1-unit.test.js
@@ -1,8 +1,6 @@
 // User Requirement 1:  Users should be able to authenticate with their Microsoft account
 
 import app from "../app.js";
-import request from "supertest";
-import { query } from "../db/connection.js";
 import { jest } from "@jest/globals";
 import { checkIfLoggedIn } from "../controllers/authControllers.js";
 
