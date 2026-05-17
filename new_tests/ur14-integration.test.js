aad48c7b02298abcbd72b80bef195634e2e73f82
angeloenrico123 <up2267576@myport.ac.uk>
2026-05-17 15:38:47 +0100

    Fix for UR14 integration

--- new_tests/ur14-integration.test.js
+++ new_tests/ur14-integration.test.js
@@ -120,7 +120,7 @@ describe('An authenticated user should be able to discuss details about the docu
             .get("/api/projects/proj-1/ai-chat")
             .set("x-test-user", "ms-john");
 
-        expect(res.status).toBe(500);
+        expect(res.status).toBe(403);
     })
 
     // Sends AI chat messages
