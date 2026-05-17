a08c39c07e75264767190e36945b29f9225428a0
Jay McKerracher <jaymckerracher@gmail.com>
2026-05-17 02:17:40 +0100

    split server.js into listen.js to split server creation and server starting for testing purposes

--- server.js
+++ server.js
@@ -3,11 +3,8 @@ import { Server } from "socket.io";
 import app from "./app.js";
 import setupSocket from "./utils/socket.js";
 
-const server = createServer(app);
-const io = new Server(server);
-setupSocket(io);
+export const server = createServer(app);
 
-// start server
-server.listen(3000, () => {
-    console.log("Server running on http://localhost:3000/ :P");
-});
+const io = new Server(server);
+
+setupSocket(io);
