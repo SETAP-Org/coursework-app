5ab14460db8b3f13f8105bc50fa997bb4a59736a
Jaketh444 <up2274964@myport.ac.uk>
2026-05-17 18:49:48 +0100

    test: add unit test for serveProjectTasks function to handle errors from underlying models

--- new_tests/serve-controllers.test.js
+++ new_tests/serve-controllers.test.js
@@ -1273,5 +1273,41 @@ describe('serveProjectTasks', () => {
         taskModels.getTasksByProjectIdModel.mockReset();
     });
 
-    
+    test('Should redirect to /error when one of the underlying models fails', async () => {
+        const { serveProjectTasks } = await import('../controllers/serveControllers.js');
+        const userModels = await import('../models/userModels.js');
+        const projectModels = await import('../models/projectModels.js');
+        const userProjectModels = await import('../models/userProjectModels.js');
+        const taskModels = await import('../models/taskModels.js');
+ 
+        // mock model to give error
+        userModels.getUserByMicrosoftIdModel.mockImplementation(() => {
+            throw new Error('DB Error');
+        });
+ 
+        const req = {
+            user: {
+                microsoftId: "myMicrosoftId"
+            },
+            params: {
+                username: "johndoe",
+                project_id: "1"
+            }
+        }
+        const res = {
+            render: jest.fn(),
+            redirect: jest.fn()
+        };
+        const next = jest.fn();
+ 
+        await serveProjectTasks(req, res, next);
+ 
+        expect(res.render).not.toHaveBeenCalled();
+        expect(res.redirect).toHaveBeenCalledWith(expect.stringContaining("/error"));
+ 
+        userModels.getUserByMicrosoftIdModel.mockReset();
+        projectModels.getProjectByIdModel.mockReset();
+        userProjectModels.getUsersByProjectId.mockReset();
+        taskModels.getTasksByProjectIdModel.mockReset();
+    });
 });
